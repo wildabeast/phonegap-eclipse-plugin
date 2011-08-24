05c9f5568143e4b6fd1a9cc0316189a6ac624638
Ryan Willoughby <ryan.willoughby@nitobi.com>
2011-08-24 16:57:25 -0700

    fixed build using httpcomponents

--- com.phonegap.sdk/src/com/phonegap/sdk/PhoneGapBuildController.java
+++ com.phonegap.sdk/src/com/phonegap/sdk/PhoneGapBuildController.java
@@ -14,6 +14,18 @@ import java.net.URL;
 import java.net.URLConnection;
 
 import org.apache.commons.codec.binary.Base64;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.*;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
 public class PhoneGapBuildController {
 	
@@ -59,73 +71,33 @@ public class PhoneGapBuildController {
 	
 	public boolean build(final String username, final String password, String appName, File appPackage) {
 		
-	  try {
-          
-          String auth = new String(Base64.encodeBase64((username + ":" + password).getBytes()));
-		  System.out.println(auth);
-		  
-		  URL url = new URL(buildUrl);
-		  URLConnection conn = url.openConnection();
-		  
-		  // this fails -- can't stop the auth window.
-		  conn.setAllowUserInteraction(false);
-		  conn.setRequestProperty("Authorization", "Basic " + auth);
-		  String boundary = Long.toHexString(System.currentTimeMillis());
-		  conn.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
-		  conn.setDoOutput(true);
-		  
-		  System.out.println("\r\n***************************************\r\n");
-          
-		  BufferedOutputStream bos = new BufferedOutputStream( conn.getOutputStream() );
-		  BufferedInputStream bis = new BufferedInputStream( new FileInputStream( appPackage ) );
-		  String dataParam = "--" + boundary + "\r\n";
-		  dataParam += "Content-Disposition: form-data; name=\"data\"\r\n";
-		  dataParam += "{\"title\":\"" + appName + "\"}\r\n\r\n";
-		  System.out.println(dataParam);
-		  bos.write(dataParam.getBytes());
-		  
-		  String fileParam = "--" + boundary + "\r\n";
-		  fileParam += "Content-Disposition: form-data; name=\"file\"; filename=\"" + appPackage.getName() + "\"\r\n";
-		  fileParam += "Content-Type: application/octet-stream\r\n";
-		  fileParam += "Content-Transfer-Encoding: binary\r\n";
-		  System.out.println(fileParam);
-		  
-		  bos.write(fileParam.getBytes());
-		  
-		  int i;
-          while ((i = bis.read()) != -1)
-          {
-             bos.write( i );
-          }
+		try{
+			DefaultHttpClient httpclient = new DefaultHttpClient();
+			HttpPost httppost = new HttpPost("https://build.phonegap.com/api/v1/apps");
+			UsernamePasswordCredentials creds = new UsernamePasswordCredentials(username, password);
+			httpclient.getCredentialsProvider().setCredentials(new AuthScope(AuthScope.ANY_HOST, AuthScope.ANY_PORT), creds);
+			
+			MultipartEntity reqEntity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
+	
+			reqEntity.addPart("data", new StringBody("{ \"title\":\"From Java Client\", \"create_method\":\"file\" }"));
+	
+			FileBody bin = new FileBody(appPackage);
+			reqEntity.addPart("file", bin );
+	
+			httppost.setEntity(reqEntity);
+	
+			System.out.println("executing request " + httppost.getRequestLine());
+			HttpResponse response = httpclient.execute(httppost);
+			HttpEntity resEntity = response.getEntity();
+	
+			if (resEntity != null) {
+				String page = EntityUtils.toString(resEntity);
+				System.out.println("PAGE :" + page);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-          System.out.println("--" + boundary + "--\r\n");
-          System.out.println("\r\n***************************************\r\n");
-          bos.write(("--" + boundary + "--\r\n").getBytes());
-          bos.flush();
-          
-          InputStream in;
-          HttpURLConnection httpConn = (HttpURLConnection)conn;
-          if (httpConn.getResponseCode() >= 400) {
-        	  in = httpConn.getErrorStream();
-          } else {
-        	  in = httpConn.getInputStream();
-          }
-          BufferedReader br=new BufferedReader(new InputStreamReader(in));
-          String read=br.readLine();
-          while(read!=null){
-        	  System.out.println(read);
-        	  read=br.readLine();
-          }
-
-          bis.close();
-          bos.close();
-          
-      
-	  } catch (MalformedURLException e) {
-		  e.printStackTrace();
-	  } catch (IOException e) {
-		  e.printStackTrace();
-	  }
 	  
 		
 	  return false;
